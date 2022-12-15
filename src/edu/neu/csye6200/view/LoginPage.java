1de4ea6754ac74fb1181081a8c7451ddb2203ee3
Shivam Thabe <thabe.s@northeastern.edu>
2022-12-15 07:38:37 -0500

    login page UI beautification

--- src/edu/neu/csye6200/view/LoginPage.java
+++ src/edu/neu/csye6200/view/LoginPage.java
@@ -6,6 +6,7 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -13,12 +14,26 @@ import javax.swing.JTextField;
 import javax.swing.JPasswordField;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.border.LineBorder;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Image;
+import javax.swing.SwingConstants;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
 
 public class LoginPage extends JFrame {
 
+	private Image img_logo = new ImageIcon(LoginPage.class.getResource("/edu/neu/csye6200/resources/daycare.png")).getImage().getScaledInstance(90, 90, Image.SCALE_SMOOTH);
+	private Image img_username = new ImageIcon(LoginPage.class.getResource("/edu/neu/csye6200/resources/admin.png")).getImage().getScaledInstance(30, 30, Image.SCALE_SMOOTH);
+	private Image img_login = new ImageIcon(LoginPage.class.getResource("/edu/neu/csye6200/resources/key.png")).getImage().getScaledInstance(30, 30, Image.SCALE_SMOOTH);
+	private Image img_password = new ImageIcon(LoginPage.class.getResource("/edu/neu/csye6200/resources/padlock.png")).getImage().getScaledInstance(30, 30, Image.SCALE_SMOOTH);
+	
 	private JPanel contentPane;
-	private JTextField textField;
-	private JPasswordField passwordField;
+	private JTextField txtUsername;
+	private JPasswordField pwdPassword;
 
 	/**
 	 * Launch the application.
@@ -41,45 +56,165 @@ public class LoginPage extends JFrame {
 	 */
 	public LoginPage() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 952, 575);
+		setBounds(100, 100, 600, 400);
 		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setBackground(new Color(0, 139, 139));
+		contentPane.setBorder(new LineBorder(new Color(0, 0, 128), 2));
 		setContentPane(contentPane);
+		setUndecorated(true);
 		contentPane.setLayout(null);
+		setLocationRelativeTo(null);
 		
-		JButton btnNewButton = new JButton("Login");
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				String passText = new String(passwordField.getPassword());
-				if(textField.getText().equals("admin") && passText.equals("1234") ) {
+		JPanel btnLogin = new JPanel();
+		btnLogin.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				String passText = new String(((JPasswordField) pwdPassword).getPassword());
+				if(txtUsername.getText().equals("admin") && passText.equals("1234") ) {
 					HomePage homePage = new HomePage();
 					homePage.setVisible(true);
 				} else {
 					JOptionPane.showMessageDialog(null, "Wrong username/password. Please try again.");
 				}
-				
+			}
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				btnLogin.setBackground(new Color(30, 60, 60));
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				btnLogin.setBackground(new Color(47, 79, 79));
+			}
+			@Override
+			public void mousePressed(MouseEvent e) {
+				btnLogin.setBackground(new Color(60, 80, 80));
+			}
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				btnLogin.setBackground(new Color(47, 79, 79));
 			}
 		});
-		btnNewButton.setBounds(354, 288, 117, 29);
-		contentPane.add(btnNewButton);
+		btnLogin.setBackground(new Color(47, 79, 79));
+		btnLogin.setBounds(180, 300, 250, 40);
+		contentPane.add(btnLogin);
+		btnLogin.setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("username");
-		lblNewLabel.setBounds(271, 185, 71, 16);
-		contentPane.add(lblNewLabel);
+		JLabel lblNewLabel_2 = new JLabel("LOG IN");
+		lblNewLabel_2.setForeground(new Color(255, 255, 255));
+		lblNewLabel_2.setFont(new Font("Lucida Grande", Font.BOLD, 14));
+		lblNewLabel_2.setBounds(112, 6, 61, 28);
+		btnLogin.add(lblNewLabel_2);
 		
-		textField = new JTextField();
-		textField.setToolTipText("Enter user name");
-		textField.setBounds(354, 180, 130, 26);
-		contentPane.add(textField);
-		textField.setColumns(10);
+		JLabel lbllogin = new JLabel("");
+		lbllogin.setHorizontalAlignment(SwingConstants.CENTER);
+		lbllogin.setBounds(69, 6, 40, 28);
+		btnLogin.add(lbllogin);
+		lbllogin.setIcon(new ImageIcon(img_login));
 		
-		JLabel lblNewLabel_1 = new JLabel("password");
-		lblNewLabel_1.setBounds(271, 232, 61, 16);
-		contentPane.add(lblNewLabel_1);
+		JLabel lblx = new JLabel("x");
+		lblx.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if(JOptionPane.showConfirmDialog(null,"Are you sure you want to close the application?", "Confirmation", JOptionPane.YES_NO_OPTION) == 0) {
+					LoginPage.this.dispose();
+				}
+			}
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				lblx.setForeground(Color.RED);
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				lblx.setForeground(Color.WHITE);
+			}
+		});
+		lblx.setForeground(new Color(255, 255, 255));
+		lblx.setHorizontalTextPosition(SwingConstants.CENTER);
+		lblx.setHorizontalAlignment(SwingConstants.CENTER);
+		lblx.setFont(new Font("Lucida Grande", Font.BOLD, 14));
+		lblx.setBounds(574, 6, 20, 20);
+		contentPane.add(lblx);
 		
-		passwordField = new JPasswordField();
-		passwordField.setToolTipText("Enter password");
-		passwordField.setBounds(354, 218, 130, 26);
-		contentPane.add(passwordField);
+		JLabel lblIconLogo = new JLabel("");
+		lblIconLogo.setHorizontalAlignment(SwingConstants.CENTER);
+		lblIconLogo.setBounds(180, 41, 250, 102);
+		contentPane.add(lblIconLogo);
+		lblIconLogo.setIcon(new ImageIcon(img_logo));
+		
+		JLabel lblIconUserName = new JLabel("New label");
+		lblIconUserName.setBounds(390, 183, 31, 40);
+		contentPane.add(lblIconUserName);
+		lblIconUserName.setIcon(new ImageIcon(img_username));
+		
+		JLabel lblIconPassword = new JLabel("New label");
+		lblIconPassword.setBounds(390, 229, 31, 40);
+		contentPane.add(lblIconPassword);
+		lblIconPassword.setIcon(new ImageIcon(img_password));
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(255, 255, 255));
+		panel.setBounds(180, 183, 250, 40);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		txtUsername = new JTextField();
+		txtUsername.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusGained(FocusEvent e) {
+				if(txtUsername.getText().equals("Username")) {
+					txtUsername.setText("");
+				} else {
+					txtUsername.selectAll();
+				}
+			}
+			@Override
+			public void focusLost(FocusEvent e) {
+				if(txtUsername.getText().equals("")) {
+					txtUsername.setText("Username");
+				}
+			}
+		});
+		txtUsername.setBorder(null);
+		txtUsername.setBounds(6, 6, 196, 26);
+		panel.add(txtUsername);
+		txtUsername.setHorizontalAlignment(SwingConstants.LEFT);
+		txtUsername.setFont(new Font("Lucida Grande", Font.PLAIN, 13));
+		txtUsername.setText("Username");
+		txtUsername.setBackground(new Color(255, 255, 255));
+		txtUsername.setToolTipText("Enter user name");
+		txtUsername.setColumns(10);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(new Color(255, 255, 255));
+		panel_1.setBounds(180, 229, 250, 40);
+		contentPane.add(panel_1);
+		panel_1.setLayout(null);
+		
+		pwdPassword = new JPasswordField();
+		pwdPassword.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusGained(FocusEvent e) {
+				if(pwdPassword.getText().equals("Password")) {
+					pwdPassword.setEchoChar('●');
+					pwdPassword.setText("");
+				} else {
+					txtUsername.selectAll();
+				}
+
+			}
+			@Override
+			public void focusLost(FocusEvent e) {
+				if(pwdPassword.getText().equals("")) {
+					pwdPassword.setText("Password");
+					pwdPassword.setEchoChar((char)0);
+				}
+			}
+		});
+		pwdPassword.setBorder(null);
+		pwdPassword.setEchoChar((char)0);
+		pwdPassword.setBounds(6, 6, 192, 26);
+		panel_1.add(pwdPassword);
+		pwdPassword.setText("Password");
+		pwdPassword.setToolTipText("password");
 	}
 }
